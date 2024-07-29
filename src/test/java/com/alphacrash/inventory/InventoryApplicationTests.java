c95fe84da1872fd511788e1cee717bee9adea2bc
alphacrash <sudhansu.ace@gmail.com>
2024-07-29 11:56:00 +0530

    Added Test Code

--- src/test/java/com/alphacrash/inventory/InventoryApplicationTests.java
+++ src/test/java/com/alphacrash/inventory/InventoryApplicationTests.java
@@ -1,7 +1,15 @@
 package com.alphacrash.inventory;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test will simply load the application context and fail if there are any issues
+	}
 }
