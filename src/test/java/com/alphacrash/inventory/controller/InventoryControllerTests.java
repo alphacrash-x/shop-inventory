57fc9d0425fdef6eab98edc1df6a524bca89c780
SUD <alphacrash@users.noreply.github.com>
2024-07-29 19:31:37 +0530

    Sonar Tests (#9)
    * Added sample test
    
    * Refactored
    
    * Updated
    
    * Added Test Code
    
    * Refactored
    
    * Refactored

---
+++ src/test/java/com/alphacrash/inventory/controller/InventoryControllerTests.java
@@ -0,0 +1,13 @@
+package com.alphacrash.inventory.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class InventoryControllerTests {
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
